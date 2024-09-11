717e751f4509469d5be6bc848a2163b51334058d
Franco Veron Peralta <francoveronperalta@gmail.com>
2024-09-11 14:29:52 -0300

    validacion informes

--- app/generar_informes.py
+++ app/generar_informes.py
@@ -2,12 +2,11 @@ from app.informes import informeGeneral, informePeorValoracion,informeMejorValor
 from app.utils import ingresoDatoInt
 def generarInformes(liCodOp=[], liNomOp=[], liLegajos=[], liNombres=[], liEmail=[], liOperaciones=[], liPuestosAtencion=[], liConformidad=[], liEConsulta=[]):
     opcion = 5
+    if len(liLegajos):
+        informeGeneral(liCodOp, liNomOp, liLegajos, liNombres, liOperaciones, liPuestosAtencion, liConformidad, liEConsulta)
+        mensaje = 'SELECCIONE UNA OPCION: \n 0. INFORME GENERAL\n 1. INFORME DE TRAMITES CON LA MEJOR VALORACION DE CONFORMIDAD \n 2. INFORME DE TRAMITES CON LA PEOR VALORACION DE CONFORMIDAD \n 3. BUSQUEDA POR ALUMNO \n 4. TERMINAR PROGRAMA \nQue desea hacer: '
     
-    informeGeneral(liCodOp, liNomOp, liLegajos, liNombres, liOperaciones, liPuestosAtencion, liConformidad, liEConsulta)
-    mensaje = 'SELECCIONE UNA OPCION: \n 0. INFORME GENERAL\n 1. INFORME DE TRAMITES CON LA MEJOR VALORACION DE CONFORMIDAD \n 2. INFORME DE TRAMITES CON LA PEOR VALORACION DE CONFORMIDAD \n 3. BUSQUEDA POR ALUMNO \n 4. TERMINAR PROGRAMA \nQue desea hacer: '
-
-    while opcion != 4: 
-        
+        while opcion != 4: 
             opcion = ingresoDatoInt(0,4, mensaje)
             if opcion == 0:
                 informeGeneral(liCodOp, liNomOp, liLegajos, liNombres, liOperaciones, liPuestosAtencion, liConformidad, liEConsulta)
